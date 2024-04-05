065a4b672e385befd9910e8a67e70d2ae0e8c498
LuciaOliveiracruz <luciaoliveiracruz2001@gmail.com>
2024-04-05 15:11:14 +0200

    doc: update README

---
+++ Cpngif.R
@@ -0,0 +1,6 @@
+rcompendium::set_credentials(given    = "Lucia",
+                             family   = "Oliveira-cruz",
+                             email    = "luciaoliveiracruz2001@gmail.com",
+                             orcid    = "",
+                             protocol = "ssh")
+rcompendium::new_compendium()
